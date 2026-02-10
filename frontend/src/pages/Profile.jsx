df588414ea40f5dc70cb58d1b59adcae3171f0aa
Abhinav kumar <abhinavbaddu01@gmail.com>
2026-02-10 16:45:13 +0530

    Apply .gitignore: remove ignored files from repo

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
     import { Button } from '@/components/ui/button'
     import { Input } from '@/components/ui/input'
     import { Label } from '@/components/ui/label'
-    import axios from "axios"
+    // import axios from "axios"
 
 
   
