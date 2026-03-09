777ad9560f5192aa8a9ada415b557102699d1d00
Abhinav kumar <abhinavbaddu01@gmail.com>
2026-03-09 11:56:21 +0530

    fix/something

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -31,4 +31,36 @@ router.post("/",async (req,res)=>{
     return res.status(201).json({ success: true, message: "Product created successfully", product });
 
 })
+router.get('/:id',async (req,res)=>{
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        return res.status(401).json({success:false,message:"id is not valid"})
+    }
+    return res.status(201).json({success:false,message:"ALL product",product})
+})
+router.put('/:id',async (req,res)=>{
+    const product = await findById(req.params.id,{
+         name: req.body.name,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: req.body.image,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured
+      })
+      if(!product){
+        return res.status(400).json({success:false,message:"All fields are required"})
+      }
+
+      return res.status(201).json({success:true,message:"ALL fields are updated",product})
+      
+})
+router.get('/:id',async (req,res)=>{
+
+})
 module.exports= router
